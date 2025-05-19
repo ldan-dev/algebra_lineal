980625d18373df2b86f02d2fa35c32960fe29fd1
Daniel Avina Neri <ld.avinaneri@ugto.mx>
2025-05-19 14:20:18 -0600

    AL gauss mane2

---
+++ proyecto-final/ui.py
@@ -0,0 +1,19 @@
+"""
+LEONARDO DANIEL AVIÑA NERI
+Fecha: 28/04/2025 (dd/mm/aaaa)
+CARRERA: LIDIA
+Universidad de Guanajuato - Campus Irapuato-Salamanca
+Correo: ld.avinaneri@ugto.mx
+UDA: 
+DESCRIPCION: 
+"""
+
+def main():
+    """  Docstring for main  """
+
+
+# functions here
+
+
+if __name__ == "__main__":
+    main()
